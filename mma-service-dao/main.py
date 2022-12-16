770e675faf9d8c42d0f42ae6cedf09acc79ec954
Diogo Neves Soares <113932801+DiogoSoares3@users.noreply.github.com>
2022-12-16 13:45:16 -0300

    Delete mma-service-dao directory

--- mma-service-dao/main.py
+++
@@ -1,6 +0,0 @@
-from controller.controller_sistema import ControladorSistema
-
-if __name__ == "__main__":
-    controlador_sistema = ControladorSistema()
-
-    controlador_sistema.inicializa_sistema()
